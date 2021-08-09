d01d5c5bade7317c5894fa223ff80a993df01f6e
Richard Schneeman <richard.schneeman+no-recruiters@gmail.com>
2021-08-08 21:21:29 -0500

    404

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,23 @@ fn handle_connection(mut stream: TcpStream ) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    let get = b"GET / HTTP/1.1\r\n";
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND \r\n\r\n";
+        let contents = fs::read_to_string("404.html").unwrap();
+
+        let response = format!("{}{}", status_line, contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+
 
     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
